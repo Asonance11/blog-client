b040b69607dc0cd821d5893ddb6be0b414733bf6
Oluwadabira <adigunsamuelaso@gmail.com>
2023-11-23 18:19:46 +0100

    Update main.tsx with router configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
+import Login from './pages/Login.tsx';
+import Signup from './pages/Signup.tsx';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+	{
+		path: '/login',
+		element: <Login />,
+	},
+	{
+		path: '/signup',
+		element: <Signup />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
