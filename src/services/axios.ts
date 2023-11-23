83dafd23d55860e63b3bcd40ca062c7921530d83
Oluwadabira <adigunsamuelaso@gmail.com>
2023-11-23 19:41:02 +0100

    Refactor axios.ts to handle error and return an empty array in case of failure

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,12 +1,14 @@
 import axios from 'axios';
+import IPost from '../types/post.type';
 
-const URL = import.meta.env.VITE_API_URL;
+const URL = import.meta.env.VITE_API_URL as string;
 
-export const getAllPosts = async () => {
+export const getAllPosts = async (): Promise<IPost[]> => {
 	try {
 		const response = await axios.get(`${URL}/posts`);
 		return response.data;
 	} catch (error) {
 		console.error(error);
+		return [];
 	}
 };
