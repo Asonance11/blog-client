4bb356cdc6293b7bcdb4aa098a38bf4eca6bfa2f
Oluwadabira <adigunsamuelaso@gmail.com>
2023-11-23 19:40:54 +0100

    Add components and import dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Toaster } from 'sonner';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Posts from './components/Posts';
+
 const App = () => {
-	return <div className="text-3xl text-green-500">App</div>;
+	return (
+		<>
+			<Toaster richColors />
+			<Navbar />
+			<Hero />
+			<Posts />
+		</>
+	);
 };
 
 export default App;
