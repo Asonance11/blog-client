86f894fe5018a9bbae1a9f3a3223da9e48cfb024
Oluwadabira <adigunsamuelaso@gmail.com>
2023-11-25 14:24:00 +0100

    Remove unused import statement

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from 'sonner';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Posts from './components/Posts';
@@ -6,7 +5,6 @@ import Posts from './components/Posts';
 const App = () => {
 	return (
 		<>
-			<Toaster richColors />
 			<Navbar />
 			<Hero />
 			<Posts />
