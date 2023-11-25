8d514497d290530138f2031abe4b7134c8258f46
Oluwadabira <adigunsamuelaso@gmail.com>
2023-11-25 21:14:41 +0100

    Creating single post page

---
+++ src/pages/Post.tsx
@@ -0,0 +1,49 @@
+import { AxiosError } from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { toast } from 'sonner';
+import Loader from '../components/Loader';
+import { getPost } from '../services/axios';
+import IPost from '../types/post.type';
+
+interface ResponseData {
+	error: string;
+}
+
+const Post: React.FC = () => {
+	const [post, setPost] = useState<IPost | null>(null);
+	const { id } = useParams();
+
+	const fetchOnePost = async () => {
+		try {
+			const response = await getPost(id);
+			setPost(response);
+		} catch (error) {
+			console.error(error);
+			const axiosError = error as AxiosError<ResponseData>;
+
+			toast.error(axiosError.response?.data?.error);
+		}
+	};
+
+	useEffect(() => {
+		fetchOnePost();
+	}, []);
+
+	return (
+		<section className="container mx-auto p-4">
+			{post ? (
+				<article className="max-w-2xl mx-auto bg-white p-8 rounded-md shadow-md">
+					<h2 className="text-3xl font-bold mb-4">{post.title}</h2>
+					<p className="text-gray-700">{post.content}</p>
+				</article>
+			) : (
+				<div className="flex items-center justify-center h-64">
+					<Loader />
+				</div>
+			)}
+		</section>
+	);
+};
+
+export default Post;
