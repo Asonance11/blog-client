e0340510a084bf030e40c6114c256c3e89d916b0
Oluwadabira <adigunsamuelaso@gmail.com>
2023-11-25 21:12:07 +0100

    Update Post component to display the username with bold font

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -7,7 +7,9 @@ const Post = (props: Proptype) => {
 	return (
 		<div className="bg-white shadow-md p-6 rounded-lg m-4 w-64">
 			<h2 className="text-xl font-bold mb-2">{props.title}</h2>
-			<p className="text-gray-600">{props.username}</p>
+			<p className="text-gray-600">
+				Created by: <span className="font-bold">{props.username}</span>
+			</p>
 		</div>
 	);
 };
