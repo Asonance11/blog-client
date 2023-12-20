ca19ef7b8d39ac009123b7f09f190d7ab5940dac
Oluwadabira <adigunsamuelaso@gmail.com>
2023-12-20 18:09:10 +0100

    refactor: Reefactored post type

--- src/types/post.type.ts
+++ src/types/post.type.ts
@@ -1,12 +1,8 @@
-import IComment from './comment.type';
-import IUser from './user.type';
-
 interface IPost {
-	_id: string;
 	title: string;
 	content: string;
-	user: IUser;
-	comments: IComment[];
+	user: string;
+	comments: string[];
 	published: boolean;
 }
 export default IPost;
