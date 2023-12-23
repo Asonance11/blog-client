671df02764bbe7fc31fa8bdb052a304a46a6cf6d
Oluwadabira <adigunsamuelaso@gmail.com>
2023-12-23 16:40:10 +0100

    trying to format the rich text but omo

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,11 +1,11 @@
 import { AxiosError } from 'axios';
+import DOMPurify from 'dompurify';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { toast } from 'sonner';
 import Loader from '../components/Loader';
 import { getPost } from '../services/axios';
 import IPost from '../types/post.type';
-
 interface ResponseData {
 	error: string;
 }
@@ -36,9 +36,11 @@ const Post: React.FC = () => {
 				<article className="max-w-2xl mx-auto bg-white p-8 rounded-md shadow-md">
 					<h2 className="text-3xl font-bold mb-4">{post.title}</h2>
 					<div
-						dangerouslySetInnerHTML={{ __html: post.content }}
+						dangerouslySetInnerHTML={{
+							__html: DOMPurify.sanitize(post.content),
+						}}
 						className="text-gray-700"
-					/>
+					></div>
 				</article>
 			) : (
 				<div className="flex items-center justify-center h-64">
