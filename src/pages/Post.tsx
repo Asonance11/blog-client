4151b55e7d144b6ee0ba914fb05c3f6262668ea6
Oluwadabira <adigunsamuelaso@gmail.com>
2023-12-23 15:10:18 +0100

    Refactor post content rendering

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -35,7 +35,10 @@ const Post: React.FC = () => {
 			{post ? (
 				<article className="max-w-2xl mx-auto bg-white p-8 rounded-md shadow-md">
 					<h2 className="text-3xl font-bold mb-4">{post.title}</h2>
-					<p className="text-gray-700">{post.content}</p>
+					<div
+						dangerouslySetInnerHTML={{ __html: post.content }}
+						className="text-gray-700"
+					/>
 				</article>
 			) : (
 				<div className="flex items-center justify-center h-64">
