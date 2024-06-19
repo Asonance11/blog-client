f204c9aefc968a9cf15e28809f7f0a61f7f5ee6d
Oluwadabira <adigunsamuelaso@gmail.com>
2024-06-19 13:55:17 +0100

    Rename to api and add login function

--- src/actions/api.ts
+++ src/actions/api.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
+import { toast } from "sonner";
 
 const url = process.env.API_URL as string;
+
+export interface IUser {
+  username: string;
+  password: string;
+}
 
 export interface IPost {
   _id: string;
@@ -11,6 +17,19 @@ export interface IPost {
   user: { username: string };
   createdAt: string;
 }
+
+// Users
+
+export const login = async (data: IUser) => {
+  try {
+    const response = await axios.post(`${url}/login`, data);
+    const { token } = response.data;
+    localStorage.setItem("token", token);
+  } catch (error) {
+    console.log("Error logging in", error);
+    toast.error("Invalid email or password");
+  }
+};
 
 export const getLatestPosts = async (): Promise<IPost[] | undefined> => {
   try {
