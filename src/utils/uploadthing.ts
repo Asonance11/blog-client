ba5a1b07cc62251ec62708f50e51ab9704143feb
Oluwadabira <adigunsamuelaso@gmail.com>
2024-06-23 13:49:22 +0100

    Add uploading images inline to uploadthing

--- src/utils/uploadthing.ts
+++ src/utils/uploadthing.ts
@@ -4,6 +4,9 @@ import {
 } from "@uploadthing/react";
 
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
+import { generateReactHelpers } from "@uploadthing/react/hooks";
+
+export const { uploadFiles } = generateReactHelpers<OurFileRouter>();
 
 export const UploadButton = generateUploadButton<OurFileRouter>();
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
